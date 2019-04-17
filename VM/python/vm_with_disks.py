efc4fb3ace543a363de6923aaa5f70e2d2389a39
bmwads <brittany.wadsworth@credera.com>
2019-04-17 12:13:20 -0500

    update netwokr

--- VM/python/vm_with_disks.py
+++ VM/python/vm_with_disks.py
@@ -71,7 +71,7 @@ def GenerateConfig(context):
                         'networkInterfaces': [{
                             'network': ''.join([COMPUTE_URL_BASE,
                                                 'projects/', project,
-                                                '/global/default/default']),
+                                                '/global/default/europe-west2']),
                            
                         }],
                         'disks': disks
