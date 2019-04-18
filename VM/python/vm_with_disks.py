e6d32a2e5a3ab98b20d27862ee694ad29a389550
bmwads <brittany.wadsworth@credera.com>
2019-04-18 12:36:43 -0500

    update tag

--- VM/python/vm_with_disks.py
+++ VM/python/vm_with_disks.py
@@ -38,8 +38,7 @@ def GenerateConfig(context):
                                            'projects/', project, '/zones/',
                                            context.properties['zone'],
                                            '/diskTypes/', disk_obj['diskType']])
-                          'tags':
-                            'items': ["test"]  
+                          'tags': ["test"]  
                       }
                      })
   disks = []
